7ee123aae63fc253a49bc76c2a6509675b0e8c3a
Trent Rand <Trentmrand@gmail.com>
2014-12-30 23:57:21 -0700

    Project files.
    + Xcode Cocoa Application files

---
+++ Apple TV Remote/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Apple TV Remote
+//
+//  Created by Trent Rand on 12/30/14.
+//  Copyright (c) 2014 applies, llc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let session: MCSession;
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
